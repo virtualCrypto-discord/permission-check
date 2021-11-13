b6ba53d7ea0fad67572b62216c3ced0773fea29c
tignear <tignear+m@gmail.com>
2021-11-13 13:15:48 +0900

    it is OK to have administrator permissions

--- index.js
+++ index.js
@@ -19,11 +19,18 @@ const rest = new REST({ version: '9' }).setToken(token);
 
     const memberRoles = guild.roles.filter(role => member.roles.includes(role.id));
     const memberPermission = memberRoles.reduce((a, b) => a | BigInt(b.permissions), 0n);
+    if (memberPermission & PermissionFlagsBits.Administrator) {
+      console.log(`OK(requester has administrator permission)`);
+      return;
+    }
     if (memberPermission & PermissionFlagsBits.ManageGuild) {
       console.log(`OK(requester has manage guild permission)`);
       return;
     }
     console.log("Error");
+    console.log(Object.entries(PermissionFlagsBits).filter(([k,v])=>{
+      return v&memberPermission;
+    }).map(e=>e[0]).join("\n"));
   } catch (error) {
     console.error(error);
   }
